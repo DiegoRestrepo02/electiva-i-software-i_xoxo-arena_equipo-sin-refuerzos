fa674e6ce8dc8ae087a99ae7948b4a171bd15b5e
Diego Restrepo <71931942+DiegoRestrepo02@users.noreply.github.com>
2026-05-16 12:47:49 -0500

    Home Provisional

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,8 +1,31 @@
+import { NavLink } from "react-router-dom";
+
 export const HomePage = () => {
     return (
         <>
             <div className="text-center py-5">
-                <h2 className="display-5 fw-bold mb-3">Home</h2>
+                {/* <h2 className="display-5 fw-bold mb-3">Home</h2> */}
+                <NavLink to="/game">
+                    <button type="button" className="btn btn-md btn-primary w-50">¡Empieza la batalla!</button>
+                </NavLink>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
             </div>
         </>
     )
